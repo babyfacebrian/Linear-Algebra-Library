6194904adde8fc2ab432b7bf2589d889cf22145e
babyfacebrian <bkalx202@gmail.com>
2018-01-17 20:12:05 -0500

    lines

---
+++ 2D_testing.py
@@ -0,0 +1,13 @@
+from math import *
+from decimal import *
+from VectorClass import *
+from LineClass import *
+
+getcontext().prec = 10
+
+
+v1 = Vector([2, 5])
+v2 = Vector([2, 1])
+
+line1 = Line(normal_vector=v1, constant_term=1)
+line2 = Line(normal_vector=v2, constant_term=1)
